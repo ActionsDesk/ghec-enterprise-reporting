80461b2717e6a83d82548f6ed6c7fa6e685f3f4f
Stefan Stölzle <stoe@github.com>
2025-03-24 06:59:13 +0100

    📦️ Update dist

--- dist/index.js
+++ dist/index.js
@@ -38164,17 +38164,23 @@ let Octokit$1 = class Octokit {
 // pkg/dist-src/generated/endpoints.js
 var Endpoints$1 = {
   actions: {
-    addRepoAccessToSelfHostedRunnerGroupInOrg: [
-      "PUT /orgs/:org/actions/runner-groups/:runner_group_id/repositories/:repository_id"
-    ],
     addSelfHostedRunnerToGroupForOrg: [
       "PUT /orgs/:org/actions/runner-groups/:runner_group_id/runners/:runner_id"
+    ],
+    createHostedRunnerForEnterprise: [
+      "POST /enterprises/:enterprise/actions/hosted-runners"
     ],
     createSelfHostedRunnerGroupForOrg: [
       "POST /orgs/:org/actions/runner-groups"
     ],
+    deleteHostedRunnerForEnterprise: [
+      "DELETE /enterprises/:enterprise/actions/hosted-runners/:hosted_runner_id"
+    ],
     deleteSelfHostedRunnerGroupFromOrg: [
       "DELETE /orgs/:org/actions/runner-groups/:runner_group_id"
+    ],
+    generateRunnerJitconfigForEnterprise: [
+      "POST /enterprises/:enterprise/actions/runners/generate-jitconfig"
     ],
     getActionsCacheUsageForEnterprise: [
       "GET /enterprises/:enterprise/actions/cache/usage"
@@ -38182,8 +38188,29 @@ var Endpoints$1 = {
     getGithubActionsDefaultWorkflowPermissionsEnterprise: [
       "GET /enterprises/:enterprise/actions/permissions/workflow"
     ],
+    getHostedRunnerForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/:hosted_runner_id"
+    ],
+    getHostedRunnersGithubOwnedImagesForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/images/github-owned"
+    ],
+    getHostedRunnersLimitsForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/limits"
+    ],
+    getHostedRunnersMachineSpecsForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/machine-sizes"
+    ],
+    getHostedRunnersPartnerImagesForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/images/partner"
+    ],
+    getHostedRunnersPlatformsForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners/platforms"
+    ],
     getSelfHostedRunnerGroupForOrg: [
       "GET /orgs/:org/actions/runner-groups/:runner_group_id"
+    ],
+    listHostedRunnersForEnterprise: [
+      "GET /enterprises/:enterprise/actions/hosted-runners"
     ],
     listRepoAccessToSelfHostedRunnerGroupInOrg: [
       "GET /orgs/:org/actions/runner-groups/:runner_group_id/repositories"
@@ -38210,6 +38237,9 @@ var Endpoints$1 = {
     setSelfHostedRunnersInGroupForOrg: [
       "PUT /orgs/:org/actions/runner-groups/:runner_group_id/runners"
     ],
+    updateHostedRunnerForEnterprise: [
+      "PATCH /enterprises/:enterprise/actions/hosted-runners/:hosted_runner_id"
+    ],
     updateSelfHostedRunnerGroupForOrg: [
       "PATCH /orgs/:org/actions/runner-groups/:runner_group_id"
     ]
@@ -38229,6 +38259,12 @@ var Endpoints$1 = {
     setAnnouncementBannerForOrg: ["PATCH /orgs/:org/announcement"]
   },
   billing: {
+    addResourceToCostCenter: [
+      "POST /enterprises/:enterprise/settings/billing/cost-centers/:cost_center_id/resource"
+    ],
+    getAllCostCenters: [
+      "GET /enterprises/:enterprise/settings/billing/cost-centers"
+    ],
     getGithubActionsBillingGhe: [
       "GET /enterprises/:enterprise/settings/billing/actions"
     ],
@@ -38238,16 +38274,37 @@ var Endpoints$1 = {
     getGithubAdvancedSecurityBillingOrg: [
       "GET /orgs/:org/settings/billing/advanced-security"
     ],
+    getGithubBillingUsageReportGhe: [
+      "GET /enterprises/:enterprise/settings/billing/usage"
+    ],
     getGithubPackagesBillingGhe: [
       "GET /enterprises/:enterprise/settings/billing/packages"
     ],
     getSharedStorageBillingGhe: [
       "GET /enterprises/:enterprise/settings/billing/shared-storage"
+    ],
+    removeResourceFromCostCenter: [
+      "DELETE /enterprises/:enterprise/settings/billing/cost-centers/:cost_center_id/resource"
     ]
   },
   codeScanning: {
     listAlertsForEnterprise: [
       "GET /enterprises/:enterprise/code-scanning/alerts"
+    ]
+  },
+  copilot: {
+    copilotMetricsForEnterprise: [
+      "GET /enterprises/:enterprise/copilot/metrics"
+    ],
+    copilotMetricsForEnterpriseTeam: [
+      "GET /enterprises/:enterprise/team/:team_slug/copilot/metrics"
+    ],
+    listCopilotSeatsForEnterprise: [
+      "GET /enterprises/:enterprise/copilot/billing/seats"
+    ],
+    usageMetricsForEnterprise: ["GET /enterprises/:enterprise/copilot/usage"],
+    usageMetricsForEnterpriseTeam: [
+      "GET /enterprises/:enterprise/team/:team_slug/copilot/usage"
     ]
   },
   enterpriseAdmin: {
@@ -38260,6 +38317,13 @@ var Endpoints$1 = {
     addSelfHostedRunnerToGroupForEnterprise: [
       "PUT /enterprises/:enterprise/actions/runner-groups/:runner_group_id/runners/:runner_id"
     ],
+    createAuditLogStream: ["POST /enterprises/:enterprise/audit-log/streams"],
+    createOrUpdateEnterpriseCustomProperties: [
+      "PATCH /enterprises/:enterprise/properties/schema"
+    ],
+    createOrUpdateEnterpriseCustomProperty: [
+      "PUT /enterprises/:enterprise/properties/schema/:custom_property_name"
+    ],
     createRegistrationTokenForEnterprise: [
       "POST /enterprises/:enterprise/actions/runners/registration-token"
     ],
@@ -38268,6 +38332,9 @@ var Endpoints$1 = {
     ],
     createSelfHostedRunnerGroupForEnterprise: [
       "POST /enterprises/:enterprise/actions/runner-groups"
+    ],
+    deleteAuditLogStream: [
+      "DELETE /enterprises/:enterprise/audit-log/streams/:stream_id"
     ],
     deleteScimGroupFromEnterprise: [
       "DELETE /scim/v2/enterprises/:enterprise/Groups/:scim_group_id"
@@ -38291,11 +38358,28 @@ var Endpoints$1 = {
       "GET /enterprises/:enterprise/actions/permissions/selected-actions"
     ],
     getAuditLog: ["GET /enterprises/:enterprise/audit-log"],
+    getAuditLogStreamKey: ["GET /enterprises/:enterprise/audit-log/stream-key"],
+    getAuditLogStreams: ["GET /enterprises/:enterprise/audit-log/streams"],
     getConsumedLicenses: ["GET /enterprises/:enterprise/consumed-licenses"],
+    getEnterpriseCustomProperties: [
+      "GET /enterprises/:enterprise/properties/schema"
+    ],
+    getEnterpriseCustomProperty: [
+      "GET /enterprises/:enterprise/properties/schema/:custom_property_name"
+    ],
+    getEnterpriseRulesetHistory: [
+      "GET /enterprises/:enterprise/rulesets/:ruleset_id/history"
+    ],
+    getEnterpriseRulesetVersion: [
+      "GET /enterprises/:enterprise/rulesets/:ruleset_id/history/:version_id"
+    ],
     getGithubActionsPermissionsEnterprise: [
       "GET /enterprises/:enterprise/actions/permissions"
     ],
     getLicenseSyncStatus: ["GET /enterprises/:enterprise/license-sync-status"],
+    getOneAuditLogStream: [
+      "GET /enterprises/:enterprise/audit-log/streams/:stream_id"
+    ],
     getProvisioningInformationForEnterpriseGroup: [
       "GET /scim/v2/enterprises/:enterprise/Groups/:scim_group_id"
     ],
@@ -38323,6 +38407,9 @@ var Endpoints$1 = {
     listProvisionedIdentitiesEnterprise: [
       "GET /scim/v2/enterprises/:enterprise/Users"
     ],
+    listPushBypassRequests: [
+      "GET /enterprises/:enterprise/bypass-requests/push-rules"
+    ],
     listRunnerApplicationsForEnterprise: [
       "GET /enterprises/:enterprise/actions/runners/downloads"
     ],
@@ -38338,6 +38425,9 @@ var Endpoints$1 = {
     listSelfHostedRunnersInGroupForEnterprise: [
       "GET /enterprises/:enterprise/actions/runner-groups/:runner_group_id/runners"
     ],
+    promoteCustomPropertyToEnterprise: [
+      "PUT /enterprises/:enterprise/properties/schema/organizations/:org/:custom_property_name/promote"
+    ],
     provisionEnterpriseGroup: ["POST /scim/v2/enterprises/:enterprise/Groups"],
     provisionEnterpriseUser: ["POST /scim/v2/enterprises/:enterprise/Users"],
     removeAllCustomLabelsFromSelfHostedRunnerForEnterprise: [
@@ -38345,6 +38435,9 @@ var Endpoints$1 = {
     ],
     removeCustomLabelFromSelfHostedRunnerForEnterprise: [
       "DELETE /enterprises/:enterprise/actions/runners/:runner_id/labels/:name"
+    ],
+    removeEnterpriseCustomProperty: [
+      "DELETE /enterprises/:enterprise/properties/schema/:custom_property_name"
     ],
     removeOrgAccessToSelfHostedRunnerGroupInEnterprise: [
       "DELETE /enterprises/:enterprise/actions/runner-groups/:runner_group_id/organizations/:org_id"
@@ -38382,18 +38475,45 @@ var Endpoints$1 = {
     updateAttributeForEnterpriseUser: [
       "PATCH /scim/v2/enterprises/:enterprise/Users/:scim_user_id"
     ],
+    updateAuditLogStream: [
+      "PUT /enterprises/:enterprise/audit-log/streams/:stream_id"
+    ],
     updateSelfHostedRunnerGroupForEnterprise: [
       "PATCH /enterprises/:enterprise/actions/runner-groups/:runner_group_id"
     ]
   },
+  hostedCompute: {
+    createNetworkConfigurationForEnterprise: [
+      "POST /enterprises/:enterprise/network-configurations"
+    ],
+    deleteNetworkConfigurationFromEnterprise: [
+      "DELETE /enterprises/:enterprise/network-configurations/:network_configuration_id"
+    ],
+    getNetworkConfigurationForEnterprise: [
+      "GET /enterprises/:enterprise/network-configurations/:network_configuration_id"
+    ],
+    getNetworkSettingsForEnterprise: [
+      "GET /enterprises/:enterprise/network-settings/:network_settings_id"
+    ],
+    listNetworkConfigurationsForEnterprise: [
+      "GET /enterprises/:enterprise/network-configurations"
+    ],
+    updateNetworkConfigurationForEnterprise: [
+      "PATCH /enterprises/:enterprise/network-configurations/:network_configuration_id"
+    ]
+  },
   orgs: {
+    createCustomOrganizationRole: ["POST /orgs/:org/organization-roles"],
     createCustomRepoRole: ["POST /orgs/:org/custom-repository-roles"],
     createCustomRole: [
       "POST /orgs/:org/custom_roles",
       {},
       {
-        deprecated: "octokit.scim.createCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---create-a-custom-role"
+        deprecated: "octokit.scim.createCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---create-a-custom-role"
       }
+    ],
+    deleteCustomOrganizationRole: [
+      "DELETE /orgs/:org/organization-roles/:role_id"
     ],
     deleteCustomRepoRole: [
       "DELETE /orgs/:org/custom-repository-roles/:role_id"
@@ -38402,7 +38522,7 @@ var Endpoints$1 = {
       "DELETE /orgs/:org/custom_roles/:role_id",
       {},
       {
-        deprecated: "octokit.scim.deleteCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---delete-a-custom-role"
+        deprecated: "octokit.scim.deleteCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---delete-a-custom-role"
       }
     ],
     getAuditLog: ["GET /orgs/:org/audit-log"],
@@ -38411,7 +38531,7 @@ var Endpoints$1 = {
       "GET /orgs/:org/custom_roles/:role_id",
       {},
       {
-        deprecated: "octokit.scim.getCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---get-a-custom-role"
+        deprecated: "octokit.scim.getCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---get-a-custom-role"
       }
     ],
     listCustomRepoRoles: ["GET /orgs/:org/custom-repository-roles"],
@@ -38419,20 +38539,27 @@ var Endpoints$1 = {
       "GET /organizations/:organization_id/custom_roles",
       {},
       {
-        deprecated: "octokit.scim.listCustomRoles() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---list-custom-repository-roles-in-an-organization"
+        deprecated: "octokit.scim.listCustomRoles() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---list-custom-repository-roles-in-an-organization"
       }
     ],
     listFineGrainedPermissions: [
       "GET /orgs/:org/fine_grained_permissions",
       {},
       {
-        deprecated: "octokit.scim.listFineGrainedPermissions() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---list-fine-grained-permissions-for-an-organization"
+        deprecated: "octokit.scim.listFineGrainedPermissions() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---list-fine-grained-permissions-for-an-organization"
       }
     ],
+    listOrganizationFineGrainedPermissions: [
+      "GET /orgs/:org/organization-fine-grained-permissions"
+    ],
+    listPushBypassRequests: ["GET /orgs/:org/bypass-requests/push-rules"],
     listRepoFineGrainedPermissions: [
       "GET /orgs/:org/repository-fine-grained-permissions"
     ],
     listSamlSsoAuthorizations: ["GET /orgs/:org/credential-authorizations"],
+    patchCustomOrganizationRole: [
+      "PATCH /orgs/:org/organization-roles/:role_id"
+    ],
     removeSamlSsoAuthorization: [
       "DELETE /orgs/:org/credential-authorizations/:credential_id"
     ],
@@ -38441,13 +38568,27 @@ var Endpoints$1 = {
       "PATCH /orgs/:org/custom_roles/:role_id",
       {},
       {
-        deprecated: "octokit.scim.updateCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#deprecated---update-a-custom-role"
+        deprecated: "octokit.scim.updateCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/orgs/custom-roles#closing-down---update-a-custom-role"
       }
     ]
   },
   repos: {
+    createEnterpriseRuleset: ["POST /enterprises/:enterprise/rulesets"],
+    deleteEnterpriseRuleset: [
+      "DELETE /enterprises/:enterprise/rulesets/:ruleset_id"
+    ],
     disableLfsForRepo: ["DELETE /repos/:owner/:repo/lfs"],
-    enableLfsForRepo: ["PUT /repos/:owner/:repo/lfs"]
+    enableLfsForRepo: ["PUT /repos/:owner/:repo/lfs"],
+    getEnterpriseRuleset: ["GET /enterprises/:enterprise/rulesets/:ruleset_id"],
+    getRepoPushBypassRequest: [
+      "GET /repos/:owner/:repo/bypass-requests/push-rules/:bypass_request_number"
+    ],
+    listRepoPushBypassRequests: [
+      "GET /repos/:owner/:repo/bypass-requests/push-rules"
+    ],
+    updateEnterpriseRuleset: [
+      "PUT /enterprises/:enterprise/rulesets/:ruleset_id"
+    ]
   },
   scim: {
     deleteUserFromOrg: [
@@ -38466,14 +38607,39 @@ var Endpoints$1 = {
     ]
   },
   secretScanning: {
+    dismissBypassResponse: [
+      "DELETE /repos/:owner/:repo/bypass-responses/secret-scanning/:bypass_response_id"
+    ],
+    getBypassRequest: [
+      "GET /repos/:owner/:repo/bypass-requests/secret-scanning/:bypass_request_number"
+    ],
     getSecurityAnalysisSettingsForEnterprise: [
-      "GET /enterprises/:enterprise/code_security_and_analysis"
+      "GET /enterprises/:enterprise/code_security_and_analysis",
+      {},
+      {
+        deprecated: "octokit.scim.getSecurityAnalysisSettingsForEnterprise() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/enterprise-admin/code-security-and-analysis#get-code-security-and-analysis-features-for-an-enterprise"
+      }
+    ],
+    listOrgBypassRequests: ["GET /orgs/:org/bypass-requests/secret-scanning"],
+    listRepoBypassRequests: [
+      "GET /repos/:owner/:repo/bypass-requests/secret-scanning"
     ],
     patchSecurityAnalysisSettingsForEnterprise: [
-      "PATCH /enterprises/:enterprise/code_security_and_analysis"
+      "PATCH /enterprises/:enterprise/code_security_and_analysis",
+      {},
+      {
+        deprecated: "octokit.scim.patchSecurityAnalysisSettingsForEnterprise() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/enterprise-admin/code-security-and-analysis#update-code-security-and-analysis-features-for-an-enterprise"
+      }
     ],
     postSecurityProductEnablementForEnterprise: [
-      "POST /enterprises/:enterprise/:security_product/:enablement"
+      "POST /enterprises/:enterprise/:security_product/:enablement",
+      {},
+      {
+        deprecated: "octokit.scim.postSecurityProductEnablementForEnterprise() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/enterprise-admin/code-security-and-analysis#enable-or-disable-a-security-feature"
+      }
+    ],
+    reviewBypassRequest: [
+      "PATCH /repos/:owner/:repo/bypass-requests/secret-scanning/:bypass_request_number"
     ]
   },
   teams: {
